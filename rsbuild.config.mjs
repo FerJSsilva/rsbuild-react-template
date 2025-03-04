85c604185edde70ed75c67ac16882fa2b60b286c
Fernando JS Silva <ferjssilva@gmail.com>
2025-03-03 22:29:27 -0300

    feat: update rsbuild.config to enable bundle analysis based on environment variable

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import dotenv from 'dotenv';
-
-// Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
 export default defineConfig({
   plugins: [pluginReact()],
-  // Desativa o bundle analyzer para desenvolvimento normal
-  performance: {
-    bundleAnalyze: false,
-  },
+  bundleAnalyze: process.env.BUNDLE_ANALYZE
+  ? {
+      analyzerMode: 'server',
+      generateStatsFile: true,
+      openAnalyzer: true,
+    }
+  : {},
   source: {
     define: {
       'process.env.RSBUILD_APP_TITLE': JSON.stringify(process.env.RSBUILD_APP_TITLE),
